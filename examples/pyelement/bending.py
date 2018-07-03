7b6dff33d5873e22b30dd13e71060a66408abb3d
Komahan Boopathy <komahan.cool@gmail.com>
2018-07-03 13:03:49 -0400

    added 2,3,4 noded beam in bending and rotating motion

--- examples/pyelement/bending.py
+++ examples/pyelement/bending.py
@@ -3,7 +3,7 @@ from tacs import TACS, elements
 import numpy as np
 import matplotlib.pyplot as plt
 
-angular_rate = 109.12
+angular_rate = 0 # 109.12
 
 def getFreqs(t, q, qdot, qddot):
     # Compute the natural frequencies
@@ -29,7 +29,8 @@ def getFreqs(t, q, qdot, qddot):
     for omega_tacs in freq:
         omega_act = np.sqrt(E*I/(rho*A*length**4))*(beta[n])**2
         n = n + 1
-        print ('%12.2f %12.2f') % (omega_tacs/109.12, omega_act/109.12)
+        print ('%12.4f %12.4f') % (omega_tacs/109.12, omega_act/109.12)
+
     return
 
 class EBBeamBending(elements.pyElement):
@@ -40,12 +41,14 @@ class EBBeamBending(elements.pyElement):
     def __init__(self, num_nodes, num_disp):
         super(EBBeamBending, self).__init__(num_nodes, num_disp)
         
-        self.E     = 70.0e9 # N/m^2
-        self.A     = 0.001 # m^2
-        self.I     = 8.33333333333333e-7 # m^4 # flap
-        self.rho   = 2700.0 # kg/m^3
-        self.speed = angular_rate #1.12
-        
+        self.E         = 70.0e9 # N/m^2
+        self.A         = 0.001 # m^2
+        self.I         = 8.33333333333333e-7 # m^4 # flap
+        self.rho       = 2700.0 # kg/m^3
+        self.speed     = angular_rate #1.12
+        self.num_nodes = num_nodes
+        self.ndof      = num_nodes*num_disp
+
         #self.b   = 0.5           # m
         #self.h   = 0.5           # m
         #self.A   = self.b*self.h # m^2
@@ -55,74 +58,131 @@ class EBBeamBending(elements.pyElement):
 
         return 
 
-    def getMassMatrix(self, l, rho, A ):        
-        # Setup MassMatrix
-        M = np.zeros([4,4])
-
-        alpha = rho*A*l/420.0
-        
-        M[0,0] = 156
-        M[0,1] = 22*l
-        M[0,2] = 54
-        M[0,3] = -13*l
-
-        M[1,0] = 22*l
-        M[1,1] = 4*l*l
-        M[1,2] = 13*l
-        M[1,3] = -3*l*l
-
-        M[2,0] = 54
-        M[2,1] = 13*l
-        M[2,2] = 156
-        M[2,3] = -22*l
-
-        M[3,0] = -13*l
-        M[3,1] = -3*l*l
-        M[3,2] = -22*l
-        M[3,3] = 4*l
-
-        return alpha*M
-
     def getTransformationMatrix(self, phi):
-        T = np.zeros([4,4])
+        
+        T = np.zeros([self.ndof,self.ndof])
+        
         c = np.cos(phi)
         s = np.sin(phi)
-        T[0,0] = c
-        T[0,1] = s
-        T[1,0] = -s
-        T[1,1] = c
-        T[2,2] = c
-        T[3,3] = c
-        T[2,3] = s
-        T[3,2] = -s
-        return np.asmatrix(T).transpose()
+        
+        if self.num_nodes == 2:
+
+            T[0,0] = c
+            T[1,1] = c
+            T[0,1] = -s
+            T[1,0] = s
+            
+            T[2,2] = c
+            T[3,3] = c
+            T[2,3] = -s
+            T[3,2] = s
+            
+        elif self.num_nodes == 3:
+
+            T[0,0] = c
+            T[1,1] = c
+            T[0,1] = -s
+            T[1,0] = s
+            
+            T[2,2] = c
+            T[3,3] = c
+            T[2,3] = -s
+            T[3,2] = s
+            
+            T[4,4] = c
+            T[5,5] = c
+            T[4,5] = -s
+            T[5,4] = s
+            
+        elif self.num_nodes == 4:
+
+            T[0,0] = c
+            T[1,1] = c
+            T[0,1] = -s
+            T[1,0] = s
+            
+            T[2,2] = c
+            T[3,3] = c
+            T[2,3] = -s
+            T[3,2] = s
+            
+            T[4,4] = c
+            T[5,5] = c
+            T[4,5] = -s
+            T[5,4] = s
+
+            T[6,6] = c
+            T[7,7] = c
+            T[6,7] = -s
+            T[7,6] = s
+                
+        return np.asmatrix(T)
     
-    def getStiffnessMatrix(self, l, E, A, I):        
-        K = np.zeros([4,4])
+    def getMassMatrix(self, L, rho, A ):        
 
-        alpha  = E*I/(l**3)
+        M = np.zeros([self.ndof, self.ndof])
+
+        if self.num_nodes == 4:
         
-        K[0,0] = 12
-        K[0,1] = 6*l
-        K[0,2] = -12
-        K[0,3] = 6*l
+            M[0,:] =  [7069*A*L*rho/80080, 263*A*L**2*rho/96096, 2889*A*L*rho/128128, -2763*A*L**2*rho/320320, 108*A*L*rho/5005, -459*A*L**2*rho/160160, 349*A*L*rho/58240, -17*A*L**2*rho/43680]
+            M[1,:] =  [263*A*L**2*rho/96096, A*L**3*rho/9240, 27*A*L**2*rho/20020, -261*A*L**3*rho/640640, 27*A*L**2*rho/22880, -9*A*L**3*rho/80080, 17*A*L**2*rho/43680, -3*A*L**3*rho/128128]
+            M[2,:] =  [2889*A*L*rho/128128, 27*A*L**2*rho/20020, 19683*A*L*rho/80080, -729*A*L**2*rho/160160, 6561*A*L*rho/91520, -729*A*L**2*rho/320320, 108*A*L*rho/5005, -27*A*L**2*rho/22880]
+            M[3,:] =  [-2763*A*L**2*rho/320320, -261*A*L**3*rho/640640, -729*A*L**2*rho/160160, 81*A*L**3*rho/20020, 729*A*L**2*rho/320320, 81*A*L**3*rho/49280, 459*A*L**2*rho/160160, -9*A*L**3*rho/80080]
+            M[4,:] =  [108*A*L*rho/5005, 27*A*L**2*rho/22880, 6561*A*L*rho/91520, 729*A*L**2*rho/320320, 19683*A*L*rho/80080, 729*A*L**2*rho/160160, 2889*A*L*rho/128128, -27*A*L**2*rho/20020]
+            M[5,:] =  [-459*A*L**2*rho/160160, -9*A*L**3*rho/80080, -729*A*L**2*rho/320320, 81*A*L**3*rho/49280, 729*A*L**2*rho/160160, 81*A*L**3*rho/20020, 2763*A*L**2*rho/320320, -261*A*L**3*rho/640640]
+            M[6,:] =  [349*A*L*rho/58240, 17*A*L**2*rho/43680, 108*A*L*rho/5005, 459*A*L**2*rho/160160, 2889*A*L*rho/128128, 2763*A*L**2*rho/320320, 7069*A*L*rho/80080, -263*A*L**2*rho/96096]
+            M[7,:] =  [-17*A*L**2*rho/43680, -3*A*L**3*rho/128128, -27*A*L**2*rho/22880, -9*A*L**3*rho/80080, -27*A*L**2*rho/20020, -261*A*L**3*rho/640640, -263*A*L**2*rho/96096, A*L**3*rho/9240]
 
-        K[1,0] = 6*l
-        K[1,1] = 4*l*l
-        K[1,2] = -6*l
-        K[1,3] = 2*l*l
+        elif self.num_nodes == 2:
 
-        K[2,0] = -12
-        K[2,1] = -6*l
-        K[2,2] = 12
-        K[2,3] = -6*l
+            M[0,:] =  [13*A*L*rho/35, 11*A*L**2*rho/210, 9*A*L*rho/70, -13*A*L**2*rho/420]
+            M[1,:] =  [11*A*L**2*rho/210, A*L**3*rho/105, 13*A*L**2*rho/420, -A*L**3*rho/140]
+            M[2,:] =  [9*A*L*rho/70, 13*A*L**2*rho/420, 13*A*L*rho/35, -11*A*L**2*rho/210]
+            M[3,:] =  [-13*A*L**2*rho/420, -A*L**3*rho/140, -11*A*L**2*rho/210, A*L**3*rho/105]
 
-        K[3,0] = 6*l
-        K[3,1] = 2*l*l
-        K[3,2] = -6*l
-        K[3,3] = 4*l*l
+        elif self.num_nodes == 3:
+
+            M[0,:] =  [523*A*L*rho/3465, 19*A*L**2*rho/2310, 4*A*L*rho/63, -8*A*L**2*rho/693, 131*A*L*rho/6930, -29*A*L**2*rho/13860]
+            M[1,:] =  [19*A*L**2*rho/2310, 2*A*L**3*rho/3465, 2*A*L**2*rho/315, -A*L**3*rho/1155, 29*A*L**2*rho/13860, -A*L**3*rho/4620]
+            M[2,:] =  [4*A*L*rho/63, 2*A*L**2*rho/315, 128*A*L*rho/315, 0, 4*A*L*rho/63, -2*A*L**2*rho/315]
+            M[3,:] =  [-8*A*L**2*rho/693, -A*L**3*rho/1155, 0, 32*A*L**3*rho/3465, 8*A*L**2*rho/693, -A*L**3*rho/1155]
+            M[4,:] =  [131*A*L*rho/6930, 29*A*L**2*rho/13860, 4*A*L*rho/63, 8*A*L**2*rho/693, 523*A*L*rho/3465, -19*A*L**2*rho/2310]
+            M[5,:] =  [-29*A*L**2*rho/13860, -A*L**3*rho/4620, -2*A*L**2*rho/315, -A*L**3*rho/1155, -19*A*L**2*rho/2310, 2*A*L**3*rho/3465]
         
-        return alpha*K
+        return M
+
+    def getStiffnessMatrix(self, L, E, A, I):        
+
+        K = np.zeros([self.ndof, self.ndof])
+        
+        if self.num_nodes == 4:
+        
+            K[0,:] =  [4539*E*I/(7*L**3), 2517*E*I/(28*L**2), -2187*E*I/(16*L**3), 12393*E*I/(56*L**2), -10935*E*I/(28*L**3), 729*E*I/(7*L**2), -13575*E*I/(112*L**3), 165*E*I/(14*L**2)]
+            K[1,:] =  [2517*E*I/(28*L**2), 6157*E*I/(385*L), -10935*E*I/(308*L**2), 148959*E*I/(6160*L), -6561*E*I/(154*L**2), 4131*E*I/(385*L), -165*E*I/(14*L**2), 6893*E*I/(6160*L)]
+            K[2,:] =  [-2187*E*I/(16*L**3), -10935*E*I/(308*L**2), 177147*E*I/(154*L**3), 6561*E*I/(44*L**2), -767637*E*I/(1232*L**3), 164025*E*I/(616*L**2), -10935*E*I/(28*L**3), 6561*E*I/(154*L**2)]
+            K[3,:] =  [12393*E*I/(56*L**2), 148959*E*I/(6160*L), 6561*E*I/(44*L**2), 45198*E*I/(385*L), -164025*E*I/(616*L**2), 490617*E*I/(6160*L), -729*E*I/(7*L**2), 4131*E*I/(385*L)]
+            K[4,:] =  [-10935*E*I/(28*L**3), -6561*E*I/(154*L**2), -767637*E*I/(1232*L**3), -164025*E*I/(616*L**2), 177147*E*I/(154*L**3), -6561*E*I/(44*L**2), -2187*E*I/(16*L**3), 10935*E*I/(308*L**2)]
+            K[5,:] =  [729*E*I/(7*L**2), 4131*E*I/(385*L), 164025*E*I/(616*L**2), 490617*E*I/(6160*L), -6561*E*I/(44*L**2), 45198*E*I/(385*L), -12393*E*I/(56*L**2), 148959*E*I/(6160*L)]
+            K[6,:] =  [-13575*E*I/(112*L**3), -165*E*I/(14*L**2), -10935*E*I/(28*L**3), -729*E*I/(7*L**2), -2187*E*I/(16*L**3), -12393*E*I/(56*L**2), 4539*E*I/(7*L**3), -2517*E*I/(28*L**2)]
+            K[7,:] =  [165*E*I/(14*L**2), 6893*E*I/(6160*L), 6561*E*I/(154*L**2), 4131*E*I/(385*L), 10935*E*I/(308*L**2), 148959*E*I/(6160*L), -2517*E*I/(28*L**2), 6157*E*I/(385*L)]
+            
+        elif self.num_nodes == 2:
+
+            K[0,:] =  [12*E*I/L**3, 6*E*I/L**2, -12*E*I/L**3, 6*E*I/L**2]
+            K[1,:] =  [6*E*I/L**2, 4*E*I/L, -6*E*I/L**2, 2*E*I/L]
+            K[2,:] =  [-12*E*I/L**3, -6*E*I/L**2, 12*E*I/L**3, -6*E*I/L**2]
+            K[3,:] =  [6*E*I/L**2, 2*E*I/L, -6*E*I/L**2, 4*E*I/L]
+
+        elif self.num_nodes == 3:
+        
+            K[0,:] =  [5092*E*I/(35*L**3), 1138*E*I/(35*L**2), -512*E*I/(5*L**3), 384*E*I/(7*L**2), -1508*E*I/(35*L**3), 242*E*I/(35*L**2)]
+            K[1,:] =  [1138*E*I/(35*L**2), 332*E*I/(35*L), -128*E*I/(5*L**2), 64*E*I/(7*L), -242*E*I/(35*L**2), 38*E*I/(35*L)]
+            K[2,:] =  [-512*E*I/(5*L**3), -128*E*I/(5*L**2), 1024*E*I/(5*L**3), 0, -512*E*I/(5*L**3), 128*E*I/(5*L**2)]
+            K[3,:] =  [384*E*I/(7*L**2), 64*E*I/(7*L), 0, 256*E*I/(7*L), -384*E*I/(7*L**2), 64*E*I/(7*L)]
+            K[4,:] =  [-1508*E*I/(35*L**3), -242*E*I/(35*L**2), -512*E*I/(5*L**3), -384*E*I/(7*L**2), 5092*E*I/(35*L**3), -1138*E*I/(35*L**2)]
+            K[5,:] =  [242*E*I/(35*L**2), 38*E*I/(35*L), 128*E*I/(5*L**2), 64*E*I/(7*L), -1138*E*I/(35*L**2), 332*E*I/(35*L)]
+        
+        return K
     
     def getInitConditions(self, u, udot, uddot, xpts):
         u[:]    = 0.01
@@ -130,8 +190,13 @@ class EBBeamBending(elements.pyElement):
         return
 
     def addResidual(self, time, res, xpts, u, udot, uddot):
-        l = xpts[3] - xpts[0]
-
+        if self.num_nodes == 4:
+            l = xpts[9] - xpts[0]
+        elif self.num_nodes == 3:
+            l = xpts[6] - xpts[0]
+        elif self.num_nodes == 2:
+            l = xpts[3] - xpts[0]
+        
         # make matrices for easy multiplication
         q = np.asmatrix(u).transpose()
         qdot = np.asmatrix(udot).transpose()
@@ -140,15 +205,10 @@ class EBBeamBending(elements.pyElement):
         # transform from local to global coordinates
         T = self.getTransformationMatrix(self.speed*time)
         
-        # Compute residual
-        K = T.transpose()*self.getStiffnessMatrix(l,
-                                                  self.E, self.A, self.I)*T
-        M = T.transpose()*self.getMassMatrix(l,
-                                             self.rho,
-                                             self.A)*T
-
+        # Compute matrices
+        K = T.transpose()*self.getStiffnessMatrix(l, self.E, self.A, self.I)*T
+        M = T.transpose()*self.getMassMatrix(l, self.rho, self.A)*T
         r = np.matmul(K, q) + np.matmul(M, qddot) - self.speed*self.speed*np.matmul(M, q)
-
         
         # Add the residual
         res += r.A1
@@ -156,29 +216,32 @@ class EBBeamBending(elements.pyElement):
         return    
 
     def addJacobian(self, time, J, alpha, beta, gamma, xpts, u, udot, uddot):
-        l = xpts[3] - xpts[0]
-        # transform from local to global coordinates
+        if self.num_nodes == 4:
+            l = xpts[9] - xpts[0]
+        elif self.num_nodes == 3:
+            l = xpts[6] - xpts[0]
+        elif self.num_nodes == 2:
+            l = xpts[3] - xpts[0]
+
+        # Transform from local to global coordinates
         T = self.getTransformationMatrix(self.speed*time)
 
-        K = T.transpose()*self.getStiffnessMatrix(l,
-                                                  self.E, self.A, self.I)*T
-        M = T.transpose()*self.getMassMatrix(l,
-                                             self.rho,
-                                             self.A)*T
-        
+        # Compute the Jacobian
+        K = T.transpose()*self.getStiffnessMatrix(l, self.E, self.A, self.I)*T
+        M = T.transpose()*self.getMassMatrix(l, self.rho, self.A)*T
         J += alpha*(K - self.speed*self.speed*M) + gamma*M 
+
         return
 
 #######################################################################
 # Create an Element
 #######################################################################
 
-nelems = 100
-length = 2.0
-dx     = length/nelems
-
+nelems    = 10
+length    = 2.0
+dx        = length/nelems
 num_disps = 2
-num_nodes = 2
+num_nodes = 3
 beam      = EBBeamBending(num_nodes, num_disps)
 
 # Verify the symmetry of stiffness matrix
@@ -200,8 +263,8 @@ for i in xrange(nelems):
     elems.append(beam)    
 
 xpts = []
-for i in xrange(nelems+1):
-    x = [dx*i, 0.0, 0.0]
+for i in xrange((num_nodes-1)*nelems+1):
+    x = [dx*i/(num_nodes-1), 0.0, 0.0]
     xpts.extend(x)
 
 ptr = [0]
@@ -210,9 +273,18 @@ for i in xrange(nelems):
 
 conn = []
 for i in xrange(nelems):
-    conn.extend([i+0, i+1])
-    
-## elems  = [beam, beam, beam, beam]
+    if num_nodes == 2:
+        idx = num_nodes-1
+        conn.extend([idx*i+0, idx*i+1])
+    elif num_nodes == 3:
+        idx = num_nodes-1
+        print [idx*i+0, idx*i+1, idx*i+2]
+        conn.extend([idx*i+0, idx*i+1, idx*i+2])    
+    elif num_nodes == 4:
+        idx = num_nodes-1
+        conn.extend([idx*i+0, idx*i+1, idx*i+2, idx*i+3])
+
+## elems  = [bar, bar, bar, bar]
 ## xpts   = [0.00 , 0.0, 0.0,
 ##           0.25 , 0.0, 0.0,
 ##           0.50 , 0.0, 0.0,
@@ -247,7 +319,7 @@ assert(nelems == ptr.shape[0]-1)
 comm = MPI.COMM_WORLD
 vars_per_node = num_disps
 creator = TACS.Creator(comm, vars_per_node)
-creator.setReorderingType(TACS.PY_NATURAL_ORDER, TACS.PY_DIRECT_SCHUR)
+creator.setReorderingType(TACS.PY_AMD_ORDER, TACS.PY_DIRECT_SCHUR)
 if comm.Get_rank() == 0:
     ids = np.arange(0, nelems, dtype=np.intc)
     creator.setGlobalConnectivity(npts, ptr, conn, ids)
@@ -261,12 +333,15 @@ tacs = creator.createTACS()
 ######################################################################
 
 steps_per_rotation = 360
-num_rotations = 10
-angular_freq = angular_rate/(2*np.pi)
-tfinal = num_rotations/angular_freq
-num_steps = num_rotations*steps_per_rotation
-order = 1
-bdf = TACS.BDFIntegrator(tacs, 0.0, tfinal, num_steps, order)
+num_rotations      = 1
+angular_freq       = angular_rate/(2*np.pi)
+if angular_freq != 0.0:
+    tfinal         = num_rotations/angular_freq
+else:
+    tfinal         = 1.0
+num_steps          = num_rotations*steps_per_rotation
+order              = 1
+bdf                = TACS.BDFIntegrator(tacs, 0.0, tfinal, num_steps, order)
 bdf.integrate()
 bdf.writeRawSolution('beam.dat', 1)
 
--- examples/pyelement/bending_matrices.py
+++ examples/pyelement/bending_matrices.py
@@ -3,7 +3,7 @@ import sympy as sym
 x = sym.Symbol('x')
 L = sym.Symbol('L')
 
-nterms = 4
+nterms = 6
 
 # Define the basis phi
 phi = sym.zeros(1,nterms)
@@ -11,12 +11,16 @@ phi[0,0] = 1
 phi[0,1] = x
 phi[0,2] = x*x
 phi[0,3] = x*x*x
+phi[0,4] = x*x*x*x
+phi[0,5] = x*x*x*x*x
 
 # Physical dofs parameters at each node (displacements, rotations)
 vi = sym.Symbol('vi')
 vpi = sym.Symbol('vpi')
 vj = sym.Symbol('vj')
 vpj = sym.Symbol('vpj')
+vk = sym.Symbol('vk')
+vpk = sym.Symbol('vpk')
 
 # Define the right hand side nodal values
 U = sym.zeros(nterms, 1)
@@ -24,13 +28,17 @@ U[0,0] = vi
 U[1,0] = vpi
 U[2,0] = vj
 U[3,0] = vpj
+U[4,0] = vk
+U[5,0] = vpk
 
 # Create the inerpolation matrix
 PHI = sym.zeros(nterms,nterms)
 PHI[0,:] = phi.subs(x,0)
 PHI[1,:] = phi.diff(x).subs(x,0)
-PHI[2,:] = phi.subs(x,L)
-PHI[3,:] = phi.diff(x).subs(x,L)
+PHI[2,:] = phi.subs(x,L/2)
+PHI[3,:] = phi.diff(x).subs(x,L/2)
+PHI[4,:] = phi.subs(x,L)
+PHI[5,:] = phi.diff(x).subs(x,L)
 
 # Invert the interpolation matrix to find alphas
 alpha = PHI.inv()*U
@@ -46,16 +54,20 @@ N[0] = v.diff(vi)
 N[1] = v.diff(vpi)
 N[2] = v.diff(vj)
 N[3] = v.diff(vpj)
+N[4] = v.diff(vk)
+N[5] = v.diff(vpk)
 
 print 'disp  shape function', N[0]
 print 'slope shape function', N[1]
 print 'disp  shape function', N[2]
 print 'slope shape function', N[3]
+print 'disp  shape function', N[4]
+print 'slope shape function', N[5]
 
 A = sym.Symbol('A')
 rho = sym.Symbol('rho')
 M = (N.transpose()*rho*A*N).integrate((x, 0, L))
-for i in xrange(4): #ndofoernode*nnodes
+for i in xrange(6): #ndofoernode*nnodes
     print ("M[%s,:] = ") % (i) , (M[i,:])[:]
 
 B = N.diff(x).diff(x)
@@ -63,5 +75,5 @@ E  = sym.Symbol('E')
 I  = sym.Symbol('I')
 K = (B.transpose()*E*I*B).integrate((x, 0, L))
 print ''
-for i in xrange(4):
+for i in xrange(6):
     print ("K[%s,:] = ") % (i) , (K[i,:])[:]
