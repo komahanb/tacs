58af33f9ef8d72b4525b8c3e7ec96ac277147e45
Komahan Boopathy <komahan.cool@gmail.com>
2018-07-14 22:11:51 -0400

    constructed a field of axial deformation automatically

--- examples/pyelement/euler_matrices.py
+++ examples/pyelement/euler_matrices.py
@@ -23,39 +23,82 @@ import sympy as sym
 ## axial_ke = axial.ke
 ## axial_ke = axial.pe
 
+# Geometry
 x = sym.Symbol('x')
 L = sym.Symbol('L')
 
-E   = sym.Symbol('E')
-G   = sym.Symbol('G')
-Ip = sym.Symbol('Ip')
-Iyy = sym.Symbol('Iyy')
-Izz = sym.Symbol('Izz')
-J   = sym.Symbol('J')
-A   = sym.Symbol('A')
-rho = sym.Symbol('rho')
+npoints = 4
 
-nterms = 2
+def nodal_dof(identifier, npoints):
+    dof = {}
+    for n in xrange(npoints):
+        key = ('%s%d') % (identifier, n+1)
+        dof[n+1] = sym.Symbol(key)
+    return dof
 
-# Define the basis phi
+def polynomial(x, N):
+    '''
+    Returns N polynomials each of from 0 to N-1.
+    [x^0, x^1, X^2, ... ,  X**(N-1)]
+    '''
+    p = {}
+    for i in xrange(N):
+        p[i+1] = x**i
+    return p
+
+def field(basis, coordinates):
+    num_basis = len(basis)
+    num_coordinates = len(coordinates)
+    assert(num_basis==num_coordinates)    
+    phi = []
+    alpha = []
+    for i in xrange(num_basis):
+        phi.append(basis[i+1])
+        alpha.append(coordinates[i+1])
+
+    # Make matrices for easy multiplication
+    phi = sym.Matrix(phi[:])
+    alpha = sym.Matrix(alpha)
+
+    # Return the dot product of basis and coordinates
+    return phi.dot(alpha)
+           
+# Construct polynomials for required number of points
+basis = polynomial(x, npoints)
+print "polynomials are poly", basis
+
+# Define nodal dofs for the field variable
+coordinates = nodal_dof('u', npoints)
+print 'coordinate are ', coordinates
+
+u = field(basis, coordinates)
+print 'field of u', u
+
+stop
+
+# Define the number of points
+npoints = 2
+
+# Define the basis phi based on number of points
 phi = sym.zeros(1,2)
 phi[0,0] = 1
 phi[0,1] = x
 
 # Physical dofs parameters at each node (displacements, rotations)
-ui     = sym.Symbol('ui')
-vi     = sym.Symbol('vi')
-wi     = sym.Symbol('wi')
-phii   = sym.Symbol('phii')
-thetai = sym.Symbol('thetai')
-psii   = sym.Symbol('psii')
+ui = sym.Symbol('ui')
+uj = sym.Symbol('uj')
 
-uj     = sym.Symbol('uj')
-vj     = sym.Symbol('vj')
-wj     = sym.Symbol('wj')
-phij   = sym.Symbol('phij')
-thetaj = sym.Symbol('thetaj')
-psij   = sym.Symbol('psij')
+## vi     = sym.Symbol('vi')
+## wi     = sym.Symbol('wi')
+## phii   = sym.Symbol('phii')
+## thetai = sym.Symbol('thetai')
+## psii   = sym.Symbol('psii')
+
+## vj     = sym.Symbol('vj')
+## wj     = sym.Symbol('wj')
+## phij   = sym.Symbol('phij')
+## thetaj = sym.Symbol('thetaj')
+## psij   = sym.Symbol('psij')
 
 # Define the right hand side nodal values
 U = sym.zeros(2, 1)
@@ -71,7 +114,7 @@ PHI[1,:] = phi.subs(x,L)
 alpha = PHI.inv()*U
 
 # Create the polynomial as a function of actual dof and x
-u     =  phi*alpha
+u =  phi*alpha
 
 # Differentiate with respect to the each rhs and get the shape functions
 print 'axial  displacement polynomial', u
@@ -80,11 +123,14 @@ print 'axial  displacement polynomial', u
 nodal_shape = {}
 nodal_shape['ui'] = u.diff(ui)[:]
 nodal_shape['uj'] = u.diff(uj)[:]
-
-
 print nodal_shape
-
 stop
+
+
+
+
+
+
 
 
 
@@ -107,6 +153,16 @@ N['thetaj'] = w.diff(thetaj)
 
 
 
+
+
+E   = sym.Symbol('E')
+G   = sym.Symbol('G')
+Ip = sym.Symbol('Ip')
+Iyy = sym.Symbol('Iyy')
+Izz = sym.Symbol('Izz')
+J   = sym.Symbol('J')
+A   = sym.Symbol('A')
+rho = sym.Symbol('rho')
 
 
 
